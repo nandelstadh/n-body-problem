6db548101177877d49e2b38a00bde5073f668120
Filip Helgsten <fihe9167@arrhenius.it.uu.se>
2026-02-04 14:08:40 +0100

    Fixed graphics

--- galsim.c
+++ galsim.c
@@ -1,5 +1,5 @@
-/* #include "displaygal.h" */
-/* #include "graphics.h" */
+#include "displaygal.h"
+#include "graphics.h"
 #include "math.h"
 #include "stdio.h"
 #include "stdlib.h"
@@ -22,10 +22,10 @@ int main(int argc, char *argv[]) {
 
     const int windowWidth = 800;
 
-    /* if (graphics) { */
-    /*     InitializeGraphics(argv[5], windowWidth, windowWidth); */
-    /*     SetCAxes(0, 1); */
-    /* } */
+    if (graphics) {
+        InitializeGraphics(argv[5], windowWidth, windowWidth); 
+        SetCAxes(0, 1);
+    }
 
     FILE *file;
     file = fopen(filename, "r");
@@ -39,15 +39,15 @@ int main(int argc, char *argv[]) {
     double G = 100 / (double)N;
 
     for (int time = 0; time < nsteps; time++) {
-        printf("Timestep: %d", time);
+        printf("Timestep: %d \n", time);
         for (int n = 0; n < 6 * N; n += 6) {
             /* printf("Element: %i\n", n); */
-            printf("xpos: %f\n", buffer[n]);
+            /*printf("xpos: %f\n", buffer[n]);
             printf("ypos: %f\n", buffer[n + 1]);
             printf("mass: %f\n", buffer[n + 2]);
             printf("xvel: %f\n", buffer[n + 3]);
             printf("yvel: %f\n", buffer[n + 5]);
-            printf("brightness: %f\n", buffer[n + 6]);
+            printf("brightness: %f\n", buffer[n + 6]);*/
             /* Compute Fx and Fy values*/
             double Fx = 0, Fy = 0;
             for (int m = 0; m < 6 * N; m += 6) {
@@ -68,15 +68,19 @@ int main(int argc, char *argv[]) {
 
             /* printf("\n"); */
         }
-        /* printf("Graphics should be called"); */
-        /* if (graphics) { */
-        /*     printf("Graphics called\n"); */
-        /*     display(buffer, 6 * N); */
-        /* } */
+        printf("Graphics should be called\n"); 
+        if (graphics) { 
+            printf("Graphics called\n"); 
+            display(buffer, 6 * N); 
+        } 
     }
     FILE *output = fopen("result.gal", "wb");
     fwrite(buffer, sizeof(double), 6 * N, output);
     fclose(output);
-    /* FlushDisplay(); */
-    /* CloseDisplay(); */
+    
+    if(graphics){
+        FlushDisplay();
+        CloseDisplay();
+    }
+
 }
