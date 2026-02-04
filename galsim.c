3a951c290c6c405b813a9aceb75ce9e5a4715a31
Anton von Nandelstadh <anton.von.nandelstadh@gmail.com>
2026-02-04 11:09:24 +0100

    Initial mainfile

--- galsim.c
+++ galsim.c
@@ -1,1 +1,53 @@
+#include "math.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
+/* Accept arguments ./galsim N filename nsteps delta_t graphics */
+/* where the input arguments have the following meaning: */
+/* N: the number of stars/particles to simulate */
+/* filename: the filename of the file to read the initial configuration from */
+/* nsteps: the number of timesteps */
+/* delta_t: the timestep ∆t */
+/* graphics: 1 or 0 meaning graphics on/off */
+
+int main(int argc, char *argv[]) {
+    int N = atoi(argv[1]);
+    char *filename = argv[2];
+    int nsteps = atoi(argv[3]);
+    double dt = atof(argv[4]);
+    int graphics = atoi(argv[5]);
+
+    FILE *file;
+    file = fopen(filename, "r");
+    double buffer[6 * N];
+
+    /*Read in the initial state*/
+    fread(buffer, sizeof(double), 6 * N, file);
+
+    double e0 = 0.001;
+    double G = 100 / (double)N;
+
+    for (int n = 0; n < 6 * N; n += 6) {
+        /* printf("Element: %i", n); */
+        /* Compute Fx and Fy values*/
+        double Fx = 0, Fy = 0;
+        for (int m = 0; m < 6 * N; m += 6) {
+            if (m == n) break;
+            double xdiff = buffer[n] - buffer[m];
+            double ydiff = buffer[n + 1] - buffer[m + 1];
+            Fx += -G * buffer[m + 2] / pow((xdiff + e0), 3) * xdiff;
+            Fy += -G * buffer[m + 2] / pow((ydiff + e0), 3) * ydiff;
+        }
+
+        /*Update buffer values*/
+        double ax = Fx / buffer[2];
+        double ay = Fy / buffer[2];
+        buffer[n + 3] += dt * ax;
+        buffer[n + 4] += dt * ay;
+        buffer[n] += dt * buffer[n + 3];
+        buffer[n + 1] += dt * buffer[n + 4];
+
+        /* printf("\n"); */
+    }
+}
