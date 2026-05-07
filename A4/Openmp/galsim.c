f28dbe5295a3c9c5beb41870b40216a1a38e07ef
Filip Helgsten <fihe9167@vitsippa.it.uu.se>
2026-05-07 10:50:28 +0200

    omp version changes

--- A4/Openmp/galsim.c
+++ A4/Openmp/galsim.c
@@ -1,9 +1,10 @@
-#include "displaygal.h"
-#include "graphics.h"
+#include "../../displaygal.h"
+#include "../../graphics.h"
 #include "math.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "omp.h"
 
 /* Accept arguments ./galsim N filename nsteps delta_t graphics */
 /* where the input arguments have the following meaning: */
@@ -19,6 +20,9 @@ int main(int argc, char* argv[]) {
     int nsteps = atoi(argv[3]);
     double dt = atof(argv[4]);
     const char graphics = atoi(argv[5]);
+    int threadcnt = atoi(argv[6]);
+
+    omp_set_num_threads(threadcnt);
 
     const int windowWidth = 800;
 
@@ -27,15 +31,26 @@ int main(int argc, char* argv[]) {
         SetCAxes(0, 1);
     }
 
-    FILE* file;
-    file = fopen(filename, "r");
-    double buffer[6 * N];
+    FILE* file = fopen(filename, "r");
+    double* buffer = malloc(sizeof(double) * 6 * N);
+    double* px = malloc(sizeof(double) * N);
+    double* py = malloc(sizeof(double) * N);
+    double* mass = malloc(sizeof(double) * N);
+    double* vx = malloc(sizeof(double) * N);
+    double* vy = malloc(sizeof(double) * N);
+    double* brightness = malloc(sizeof(double) * N);
+    double* axup = calloc((size_t)threadcnt * N, sizeof(double));
+    double* ayup = calloc((size_t)threadcnt * N, sizeof(double));
+
+    if (file == NULL || buffer == NULL || px == NULL || py == NULL || mass == NULL || vx == NULL || vy == NULL || brightness == NULL || axup == NULL || ayup == NULL) {
+        perror("alloc/open");
+        return 1;
+    }
 
     /*Read in the initial state*/
     fread(buffer, sizeof(double), 6 * N, file);
     fclose(file);
 
-    double px[N], py[N], mass[N], vx[N], vy[N], brightness[N], ax[N], ay[N];
 #pragma omp parallel for
     for (int i = 0; i < N; i++) {
         px[i] = buffer[6 * i];
@@ -44,59 +59,77 @@ int main(int argc, char* argv[]) {
         vx[i] = buffer[6 * i + 3];
         vy[i] = buffer[6 * i + 4];
         brightness[i] = buffer[6 * i + 5];
-        ax[i] = ay[i] = 0;
     }
 
     double e0 = 0.001;
     double G = 100 / (double)N;
+    double* ax = calloc(N, sizeof(double));
+    double* ay = calloc(N, sizeof(double));
 
-    for (int time = 0; time < nsteps; time++) {
-#pragma omp parallel for reduction(+ : ax, ay)
-        for (int n = 0; n < N; n++) {
-            double px_n = px[n];
-            double py_n = py[n];
-            double mass_n = mass[n];
-            double axn = 0, ayn = 0;
-            for (int m = n + 1; m < N; m++) {
-                double xdiff = px_n - px[m];
-                double ydiff = py_n - py[m];
-                double r = sqrt(xdiff * xdiff + ydiff * ydiff) + e0;
-                double r3 = r * r * r;
-                double force_x = -G * xdiff / r3;
-                double force_y = -G * ydiff / r3;
-                axn += mass[m] * force_x;
-                ayn += mass[m] * force_y;
-                ax[m] -= mass_n * force_x;
-                ay[m] -= mass_n * force_y;
-            }
-            ax[n] += axn;
-            ay[n] += ayn;
-        }
+#pragma omp parallel
+    {
+        int tid = omp_get_thread_num();
+        double* ax_local = axup + ((size_t)tid * N);
+        double* ay_local = ayup + ((size_t)tid * N);
 
-#pragma omp parallel for
-        for (int n = 0; n < N; n++) {
-            vx[n] += dt * ax[n];
-            vy[n] += dt * ay[n];
-            px[n] += dt * vx[n];
-            py[n] += dt * vy[n];
-        }
-        if (graphics) {
-            for (int i = 0; i < N; i++) {
-                buffer[6 * i] = px[i];
-                buffer[6 * i + 1] = py[i];
-                buffer[6 * i + 2] = mass[i];
-                buffer[6 * i + 3] = vx[i];
-                buffer[6 * i + 4] = vy[i];
-                buffer[6 * i + 5] = brightness[i];
+        for (int time = 0; time < nsteps; time++) {
+#pragma omp for
+            for (int n = 0; n < N; n++) {
+                double px_n = px[n];
+                double py_n = py[n];
+                double mass_n = mass[n];
+                double axn = 0, ayn = 0;
+                for (int m = n + 1; m < N; m++) {
+                    double xdiff = px_n - px[m];
+                    double ydiff = py_n - py[m];
+                    double r = sqrt(xdiff * xdiff + ydiff * ydiff) + e0;
+                    double r3 = r * r * r;
+                    double force_x = -G * xdiff / r3;
+                    double force_y = -G * ydiff / r3;
+                    axn += mass[m] * force_x;
+                    ayn += mass[m] * force_y;
+                    ax_local[m] -= mass_n * force_x;
+                    ay_local[m] -= mass_n * force_y;
+                }
+                ax_local[n] += axn;
+                ay_local[n] += ayn;
             }
-            display(buffer, 6 * N);
-        }
-#pragma omp parallel for
-        for (int n = 0; n < N; n++) {
-            ax[n] = 0;
-            ay[n] = 0;
+
+#pragma omp single
+            {
+                for (int n = 0; n < N; n++) {
+                    double ax_total = 0;
+                    double ay_total = 0;
+                    for (int k = 0; k < threadcnt; k++) {
+                        ax_total += axup[(size_t)k * N + n];
+                        ay_total += ayup[(size_t)k * N + n];
+                    }
+                    ax[n] = ax_total;
+                    ay[n] = ay_total;
+                    vx[n] += dt * ax[n];
+                    vy[n] += dt * ay[n];
+                    px[n] += dt * vx[n];
+                    py[n] += dt * vy[n];
+                }
+
+                if (graphics) {
+                    for (int i = 0; i < N; i++) {
+                        buffer[6 * i] = px[i];
+                        buffer[6 * i + 1] = py[i];
+                        buffer[6 * i + 2] = mass[i];
+                        buffer[6 * i + 3] = vx[i];
+                        buffer[6 * i + 4] = vy[i];
+                        buffer[6 * i + 5] = brightness[i];
+                    }
+                    display(buffer, 6 * N);
+                }
+
+                memset(axup, 0, sizeof(double) * (size_t)threadcnt * N);
+                memset(ayup, 0, sizeof(double) * (size_t)threadcnt * N);
+            }
         }
     }
+
 #pragma omp parallel for
     for (int i = 0; i < N; i++) {
         buffer[6 * i] = px[i];
@@ -114,4 +147,16 @@ int main(int argc, char* argv[]) {
         FlushDisplay();
         CloseDisplay();
     }
+
+    free(buffer);
+    free(px);
+    free(py);
+    free(mass);
+    free(vx);
+    free(vy);
+    free(brightness);
+    free(ax);
+    free(ay);
+    free(axup);
+    free(ayup);
 }
